d065f235396b1caa85aeef39da735ce32e26deaf
Johann <jl.01@icloud.com>
2019-01-11 12:30:32 +0100

    method added: removeObject in Object.class

--- Objects.java
+++ Objects.java
@@ -3,17 +3,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Objects here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Johann
+ * @version (a version numbedfgdgfshdgfhr or a date)
  */
 public class Objects extends Actor
 {
-    /**
-     * Act - do whatever the Objects wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act() 
     {
-        // Add your action code here.
-    }    
+    }
+    
+    public void remove() {
+        getWorld().removeObject(this);    
+    }
 }
