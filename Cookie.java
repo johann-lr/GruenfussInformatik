5873b27998f5e5fc8d70f3a386f980a6e4cac4f2
Johann <jl.01@icloud.com>
2019-01-11 12:47:55 +0100

    added cookie.class

---
+++ Cookie.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+
+
+// objekt beim zusammentreffen entfernen, siehe Krabben-Szenario
+
+public class Cookie extends Objects
+{
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
